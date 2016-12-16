d09fad0013eeb5e98ee593ee35fc7bdc506c9525
Toxagin <toxagin@gmail.com>
2016-12-16 23:11:32 +0300

    sites health under control now

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 17_sites_monitoring
+
+## Synopsis
+
+Simple site checker. Help you to understand site is respond with status 200 and domain name is not expired.
+
+## Quick start
+
+ - Install all requirements from file `pip3 install -r requirements.txt`
+ - Run check_sites_health.py `python3.5 check_sites_health.py`
+ - **Input**: path to the file with urls of the site pages. File must have format .txt and each url must stand on separate line.
+ - **Output**: List of urls of sites and additional information about response and domain expiration.
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,44 @@
+import whois
+import requests
+import datetime
+from urllib.parse import urlparse
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as file:
+        for line in file:
+            yield line.strip()
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get(url)
+    return response.status_code == requests.codes.ok
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois(domain_name)
+    if isinstance(domain_info.expiration_date,list):
+        return domain_info.expiration_date[0]
+    return domain_info.expiration_date
+
+def extract_domain_name(url):
+    parsed_uri = urlparse(url)
+    return parsed_uri.netloc
 
 if __name__ == '__main__':
-    pass
+    for url in load_urls4check('../checking_urls.txt'):
+        print(url)
+        if is_server_respond_with_200(url):
+            print(' - response OK')
+        else:
+            print(' - response Error')
+        domain_name = extract_domain_name(url)
+        expiration = get_domain_expiration_date(domain_name)
+        td_month = datetime.timedelta(days=30)
+        future_day = datetime.date.today() + td_month
+        if future_day <= expiration.date():
+            print(' - domain paid at least 30 days ({0})'.format(
+                expiration.strftime('%Y-%m-%d')
+            ))
+        else:
+            print(' - must be paid before {0}'.format(
+                expiration.strftime('%Y-%m-%d')
+            ))
+        print()
